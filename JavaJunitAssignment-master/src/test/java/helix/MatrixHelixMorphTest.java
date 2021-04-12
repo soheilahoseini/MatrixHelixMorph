b95a8ac9f87df8f0a0671e7c78ac043722061c3e
Soheila Hoseini <hoseinis@student.douglascollege.ca>
2021-04-11 20:19:21 -0700

    test3*6 added

--- JavaJunitAssignment-master/src/test/java/helix/MatrixHelixMorphTest.java
+++ JavaJunitAssignment-master/src/test/java/helix/MatrixHelixMorphTest.java
@@ -86,4 +86,11 @@ public class MatrixHelixMorphTest {
 //1   2   3   4  5   6
 //        7   8   9  10  11  12
 //        13  14  15 16  17  18
+
+    @Test
+    public void test3x6() {
+        int[][] inMatrix  = { {1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18} };
+        int[][] expOutput = { {1,2,3,4,5,6},{12,18,17,16,15,14},{13,7,8,9,10,11} };
+        assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
+    }
 }
